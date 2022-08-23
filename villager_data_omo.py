59a87895112350541a31376df3c90d0b72aedb41
Carina Ogbat <carinawrightogbat@gmail.com>
2022-08-23 10:50:24 -0700

    created new file to practice lab again

---
+++ villager_data_omo.py
@@ -0,0 +1,30 @@
+def all_species(filename):
+    """Takes in the name of a data file and returns unique species found"""
+    #set[str]
+    file = open(filename)
+
+
+def get_villagers_by_species(filename, search_string='All'):
+    """Takes in name of file and name of species, returns names of all villagers of that species
+    in alphabetical order"""
+    #list[str]
+
+
+def all_names_by_hobby(filename):
+    """Returns list of villagers with names grouped by hobby"""
+    #should be list with 6 lists list[list[string]]
+
+
+def all_data(filename):
+    """Return all the data in file as a list of tuples"""
+    #list[tuple[str]]
+
+
+def find_motto(filename, villager_name):
+    """Take in the name of the villager and return villager's motto or none is unable to find"""
+    #str
+    
+
+def find_likeminded_villagers(filename, villager_name):
+    """Take in the file and name of villager, return a set of villagers with the same personality"""
+    #set[str]
